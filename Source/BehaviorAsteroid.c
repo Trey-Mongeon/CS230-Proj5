824cf09c6164401539bb63867b096c9ef7c2f252
Trey <mongeontrey@gmail.com>
2025-03-11 20:09:04 -0700

    did woek

--- Source/BehaviorAsteroid.c
+++ Source/BehaviorAsteroid.c
@@ -11,14 +11,53 @@
 
 #include "stdafx.h"
 #include "BehaviorAsteroid.h"
+#include "Behavior.h"
+#include "Random.h"
+#include "Entity.h"
+#include "Collider.h"
+#include "Teleporter.h"
+#include "Vector2D.h"
+#include "Transform.h"
+#include "Physics.h"
 
 //------------------------------------------------------------------------------
 // Private Constants:
 //------------------------------------------------------------------------------
 
+enum AsteroidState
+{
+	cAsteroidInvalid = -1,
+	cAsteroidIdle
+};
+
+typedef enum
+{
+	cAsteroidOriginTlc,
+	cAsteroidOriginTrc,
+	cAsteroidOriginBlc,
+	cAsteroidOriginBrc,
+	cAsteroidOriginCount,
+
+} AsteroidOrigin;
+
+
+static const float asteroidSpeedMin = 50.0f;
+static const float asteroidSpeedMax = 100.0f;
+
+
 //------------------------------------------------------------------------------
 // Private Structures:
 //------------------------------------------------------------------------------
+
+typedef struct BehaviorAsteroid
+{
+	// Inherit the base behavior structure.
+	Behavior	base;
+
+	// Add asteroid-specific behavior variables.
+	AsteroidOrigin	origin;
+
+} BehaviorAsteroid;
 
 //------------------------------------------------------------------------------
 // Public Variables:
@@ -28,15 +67,159 @@
 // Private Variables:
 //------------------------------------------------------------------------------
 
+BehaviorAsteroid* behaviorAsteroid;
+
 //------------------------------------------------------------------------------
 // Private Function Declarations:
 //------------------------------------------------------------------------------
+
+static void BehaviorAsteroidInit(Behavior*);
+static void BehaviorAsteroidUpdate(Behavior*, float dt);
+static void BehaviorAsteroidExit(Behavior* behavior);
+static void BehaviorAsteroidSetPosition(BehaviorAsteroid* behavior);
+static void BehaviorAsteroidSetVelocity(BehaviorAsteroid* behavior);
+static void BehaviorAsteroidCollisionHandler(Entity* entity1, const Entity* entity2);
+
 
 //------------------------------------------------------------------------------
 // Public Functions:
 //------------------------------------------------------------------------------
 
+// Dynamically allocate a new (Asteroid) behavior component.
+// (Hint: Use calloc() to ensure that all member variables are initialized to 0.)
+Behavior* BehaviorAsteroidCreate(void)
+{
+	BehaviorAsteroid* asteroidBehavior = calloc(1, sizeof(Behavior));
+
+	if (asteroidBehavior)
+	{
+		asteroidBehavior->base.memorySize = sizeof(Behavior);
+		asteroidBehavior->base.onExit = BehaviorAsteroidExit;
+		asteroidBehavior->base.onInit = BehaviorAsteroidInit;
+		asteroidBehavior->base.onUpdate = BehaviorAsteroidUpdate;
+		asteroidBehavior->base.stateCurr = cAsteroidInvalid;
+		asteroidBehavior->base.stateNext = cAsteroidInvalid;
+
+		return (Behavior*)asteroidBehavior;
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
 //------------------------------------------------------------------------------
 // Private Functions:
 //------------------------------------------------------------------------------
 
+
+static void BehaviorAsteroidInit(Behavior* behavior)
+{
+	behaviorAsteroid = (BehaviorAsteroid*)behavior;
+
+	if (behaviorAsteroid->base.stateCurr == cAsteroidIdle)
+	{
+		behaviorAsteroid->origin = RandomRange(0, 3);
+
+		BehaviorAsteroidSetPosition(behaviorAsteroid);
+		BehaviorAsteroidSetVelocity(behaviorAsteroid);
+
+		Collider* asteroidCollider = EntityGetCollider(behaviorAsteroid->base.parent);
+
+		if (asteroidCollider)
+		{
+			ColliderSetCollisionHandler(asteroidCollider, BehaviorAsteroidCollisionHandler);
+		}
+	}
+
+}
+
+
+static void BehaviorAsteroidUpdate(Behavior* behavior, float dt)
+{
+	TeleporterUpdateEntity(behavior->parent);
+	UNREFERENCED_PARAMETER(dt);
+}
+
+
+static void BehaviorAsteroidExit(Behavior* behavior)
+{
+	UNREFERENCED_PARAMETER(behavior);
+}
+
+
+static void BehaviorAsteroidSetPosition(BehaviorAsteroid* behavior)
+{
+	DGL_Vec2 window = DGL_Window_GetSize();
+
+	Transform* entityTransform = EntityGetTransform(behaviorAsteroid->base.parent);
+
+	DGL_Vec2 asteroidPosition = *TransformGetTranslation(entityTransform);
+
+	switch (behavior->origin)
+	{
+	case cAsteroidOriginTlc:
+		asteroidPosition.x = -window.x;
+		asteroidPosition.y = window.y;
+		break;
+
+	case cAsteroidOriginTrc:
+		asteroidPosition.x = window.x;
+		asteroidPosition.y = window.y;
+		break;
+
+	case cAsteroidOriginBlc:
+		asteroidPosition.x = -window.x;
+		asteroidPosition.y = -window.y;
+		break;
+
+	case cAsteroidOriginBrc:
+		asteroidPosition.x = window.x;
+		asteroidPosition.y = -window.y;
+		break;
+	}
+
+	TransformSetTranslation(entityTransform, &asteroidPosition);
+}
+
+
+static void BehaviorAsteroidSetVelocity(BehaviorAsteroid* behavior)
+{
+	float angle = 0;
+	switch (behavior->origin)
+	{
+	case cAsteroidOriginTlc:
+		angle = RandomRangeFloat(-10, -80);
+		break;
+
+	case cAsteroidOriginTrc:
+		angle = RandomRangeFloat(-100, -170);
+		break;
+
+	case cAsteroidOriginBlc:
+		angle = RandomRangeFloat(10, 80);
+		break;
+
+	case cAsteroidOriginBrc:
+		angle = RandomRangeFloat(100, 170);
+		break;
+	}
+
+	Physics* entityPhysics = EntityGetPhysics(behaviorAsteroid->base.parent);
+
+	DGL_Vec2 vecFromAngle;
+	Vector2DFromAngleDeg(&vecFromAngle, angle);
+
+	PhysicsSetVelocity(entityPhysics, &vecFromAngle);
+
+}
+
+
+static void BehaviorAsteroidCollisionHandler(Entity* entity1, const Entity* entity2)
+{
+	if (entity1 && entity2)
+	{
+		//ScoreSystemIncreaseScore(20);
+		EntityDestroy(entity1);
+	}
+}
--- Source/ScoreSystem.c
+++ Source/ScoreSystem.c
@@ -2,7 +2,7 @@
 //
 // File Name:	ScoreSystem.c
 // Author(s):	Trey Mongeon (tmongeon), Doug Schilling (dschilling)
-// Project:		Project 5
+// Project:		Project 2
 // Course:		CS230S25
 //
 // Copyright © 2025 DigiPen (USA) Corporation.
@@ -10,6 +10,9 @@
 //------------------------------------------------------------------------------
 
 #include "stdafx.h"
+#include <Windows.h>
+
+#include "BaseSystem.h"
 #include "ScoreSystem.h"
 
 //------------------------------------------------------------------------------
@@ -20,23 +23,89 @@
 // Private Structures:
 //------------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
-// Public Variables:
-//------------------------------------------------------------------------------
+typedef struct ScoreSystem
+{
+	// WARNING: The base class must always be included first.
+	BaseSystem	base;
+
+	// Add any system-specific variables second.
+	unsigned score;
+	unsigned highScore;
+	unsigned waveCount;
+
+
+} ScoreSystem;
 
 //------------------------------------------------------------------------------
-// Private Variables:
+// Public Variables:
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
 // Private Function Declarations:
 //------------------------------------------------------------------------------
 
+//  System-specific State functions:
+static bool ScoreSystemInit(void);
+static void ScoreSystemUpdate(float dt);
+static void ScoreSystemRender(void);
+static void ScoreSystemExit(void);
+
+//------------------------------------------------------------------------------
+// Private Variables:
+//------------------------------------------------------------------------------
+
+static ScoreSystem instance =
+{
+	{ "ScoreSystem", ScoreSystemInit, ScoreSystemUpdate, ScoreSystemRender, ScoreSystemExit},
+};
+
 //------------------------------------------------------------------------------
 // Public Functions:
 //------------------------------------------------------------------------------
 
+// Get the instance of the Score System.
+// Returns:
+//	 Pointer to the abstract BaseSystem for this derived System.
+BaseSystem* ScoreSystemGetInstance()
+{
+	// Initialize any system-specific variables here:
+
+	// Return a reference to the instance of this system.
+	return (BaseSystem*)&instance;
+}
+
 //------------------------------------------------------------------------------
 // Private Functions:
 //------------------------------------------------------------------------------
+
+// Initialize the Score System.
+// Returns:
+//   Return "true" if the system was initialized successfully, otherwise return "false".
+static bool ScoreSystemInit(void)
+{
+	// Return false if the system was NOT initialized successfully.
+
+	// Return true if the system was initialized successfully.
+	return true;
+}
+
+// Update the Score System.
+// Params:
+//	 dt = Change in time (in seconds) since the last game loop.
+static void ScoreSystemUpdate(float dt)
+{
+	// Tell the compiler that the 'dt' variable is unused.
+	UNREFERENCED_PARAMETER(dt);
+}
+
+// Render any objects associated with the Score System.
+static void ScoreSystemRender(void)
+{
+}
+
+// Shutdown the Score System.
+//   (HINT: Free any allocated resources and/or close any opened files).
+static void ScoreSystemExit(void)
+{
+}
 
